5fc337c104113138dce267488ce87cbfddf4b470
Mengjuei Hsieh <mhsieh@pacb.com>
2015-12-03 22:37:06 +0000

    update config and do-(un)install

--- mk/config.mk
+++ mk/config.mk
@@ -1,6 +1,8 @@
 # Global overrides
 GIT       = git
 SED       = sed
+MD5SUM    = md5sum
+SHA1SUM   = sha1sum
 WORKSPACE = $(realpath ../../..)/workspace
 PREFIX   ?= $(realpath ../../..)/deployment
 CFLAGS    = -fPIC -static-libgcc
@@ -10,4 +12,9 @@ default:
 wscheck:
 	@mkdir -p $(WORKSPACE) || exit 1
 pfcheck:
-	@mkdir -p "$(PREFIX)" || exit 1
+	@mkdir -p "$(PREFIX)/lib"            || exit 1
+	@mkdir -p "$(PREFIX)/lib/pkgconfig"  || exit 1
+	@mkdir -p "$(PREFIX)/share"          || exit 1
+	@mkdir -p "$(PREFIX)/share/man"      || exit 1
+	@mkdir -p "$(PREFIX)/share/man/man3" || exit 1
+	@mkdir -p "$(PREFIX)/include"        || exit 1
