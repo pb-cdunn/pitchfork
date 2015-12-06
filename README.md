4c1fbed0c6d7cd69cf6cad874e4663b071f410d3
Mengjuei <mjhsieh@gmail.com>
2015-12-06 21:40:28 +0000

    possibly the dumpest pkg system
    $(PREFIX)/var/pkg/$(_NAME) contains a list of file names installed
    so when we do-uninstall, we just remove the files, sorry about the
    mess of directories, we don't care.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pitchfork
 Prototyping github source building
+
+## style guide
+
+1. use ```cd $(PREFIX)/lib && ln -s foo.so.1 foo.so``` instead of ```ln -s $(PREFIX)/lib/foo.so.1 $(PREFIX)/lib/foo.so```, since they are different.
--- mk/config.mk
+++ mk/config.mk
@@ -1,18 +1,22 @@
 # Global overrides
-SHELL     = /bin/bash
-CC        = gcc
-CXX       = g++
-GIT       = git
-SED       = sed
-CURL      = curl
-MD5SUM    = md5sum
-SHA1SUM   = sha1sum
-PFHOME   := $(realpath ../../..)
-WORKSPACE = $(PFHOME)/workspace
-PREFIX   ?= $(PFHOME)/deployment
-STAGING  ?= $(PFHOME)/staging
-CFLAGS    = -fPIC -static-libgcc
-CFLAGS   += -I$(PREFIX)/include -L$(PREFIX)/lib
+SHELL            = /bin/bash
+CC               = gcc
+CXX              = g++
+GIT              = git
+SED              = sed
+CURL             = curl
+MD5SUM           = md5sum
+SHA1SUM          = sha1sum
+PFHOME          := $(realpath ../../..)
+WORKSPACE        = $(PFHOME)/workspace
+PREFIX          ?= $(PFHOME)/deployment
+STAGING         ?= $(PFHOME)/staging
+CFLAGS           = -fPIC -static-libgcc
+CFLAGS          += -I$(PREFIX)/include -L$(PREFIX)/lib
+PATH             = $(PREFIX)/bin:$$PATH
+LD_LIBRARY_PATH  = $(PREFIX)/lib:$$LD_LIBRARY_PATH
+export PATH
+export LD_LIBRARY_PATH
 
 include $(PFHOME)/mk/sanity.mk
 
@@ -20,13 +24,13 @@ default:
 	@echo "[INFO] nothing is done."
 wscheck:
 	@mkdir -p $(WORKSPACE) || exit 1
-stcheck:
+_stcheck:
 	@mkdir -p "$(STAGING)"              || exit 1
-pfcheck: stcheck
+pfcheck: _stcheck
 	@mkdir -p "$(PREFIX)/bin"           || exit 1
 	@mkdir -p "$(PREFIX)/lib"           || exit 1
 	@mkdir -p "$(PREFIX)/lib/pkgconfig" || exit 1
 	@mkdir -p "$(PREFIX)/share"         || exit 1
-	@mkdir -p "$(PREFIX)/share/man"     || exit 1
 	@mkdir -p "$(PREFIX)/include"       || exit 1
 	@mkdir -p "$(PREFIX)/var/pkg"       || exit 1
+#	@mkdir -p "$(PREFIX)/share/man"     || exit 1
