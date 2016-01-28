b99078cfa5f53e76ef3093cd9f8a8f0fcd1b5df9
Mengjuei Hsieh <mhsieh@pacb.com>
2016-01-28 10:58:51 -0800

    linux might misses curl

--- mk/sanity.mk
+++ mk/sanity.mk
@@ -1,6 +1,7 @@
 #
 _tmpvar:=$(if $(shell which $(CC)),exists,$(error "unable to run $(CC), consider doing yum install gcc"))
 _tmpvar:=$(if $(shell which arch),exists,$(error "unable to run arch, consider doing yum install coreutils"))
+_tmpvar:=$(if $(shell which curl),exists,$(error "unable to run curl, consider doing yum install curl"))
 _tmpvar:=$(if $(shell which unzip),exists,$(error "unable to run unzip, consider doing yum install unzip"))
 ifeq ($(OPSYS),Darwin)
 _tmpvar:=$(if $(shell which $(MD5SUM)),exists,$(error "unable to run md5sum, consider doing brew install md5sha1sum"))
