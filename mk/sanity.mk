b6da2ed65ff11b2e83d1cc160074aaa5b17447a3
Mengjuei <mjhsieh@gmail.com>
2016-02-03 01:03:23 -0800

    rsync check

--- mk/sanity.mk
+++ mk/sanity.mk
@@ -3,6 +3,7 @@ _tmpvar:=$(if $(shell which $(CC)),exists,$(error "unable to run $(CC), consider
 _tmpvar:=$(if $(shell which arch),exists,$(error "unable to run arch, consider doing yum install coreutils"))
 _tmpvar:=$(if $(shell which curl),exists,$(error "unable to run curl, consider doing yum install curl"))
 _tmpvar:=$(if $(shell which git),exists,$(error "unable to run git, consider doing yum install git"))
+_tmpvar:=$(if $(shell which rsync),exists,$(error "unable to run rsync, consider doing yum install rsync"))
 ifeq ($(OPSYS),Darwin)
 _tmpvar:=$(if $(shell which $(MD5SUM)),exists,$(error "unable to run md5sum, consider doing brew install md5sha1sum"))
 else
