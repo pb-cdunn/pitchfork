9941693f9952fe742e304e09364ee35273c2a8a7
Mengjuei <mjhsieh@gmail.com>
2016-03-04 22:35:30 -0800

    update MarkupSafe and travis test

--- bin/travistest.sh
+++ bin/travistest.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 . ${HOME}/local/setup-env.sh
+set -e
+
+bam2bax -h
+bamSieve -h
+bax2bam -h
 blasr -h
-pbsmrtpipe -h
 ccs -h
+dataset -h
 pbalign -h
-rtnV=$? 
-echo $rtnV
-exit $rtnV
+pbindex -h
+pbmerge -h
+pbservice -h
+pbsmrtpipe -h
+pbtestkit-runner -h
+pbtools-runner -h
+pbvalidate -h
+sawriter
