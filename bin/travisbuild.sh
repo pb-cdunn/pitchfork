5866fa9325ec136b7fbe9c30199f2ca667bd85bd
Christopher Dunn <cdunn@pacificbiosciences.com>
2016-03-07 08:28:35 -0800

    Try to debug pbbam build error. Maybe ran out of resources? Limiting -j might help.

--- bin/travisbuild.sh
+++ bin/travisbuild.sh
@@ -11,4 +11,4 @@ test -e $HOME/distfiles/boost_1_56_0.tar.gz \
 tar zxf $HOME/distfiles/boost_1_56_0.tar.gz --strip=1 -C $HOME/boost_1_56_0/include/ boost_1_56_0/boost
 cp mk/travis.mk settings.mk
 make init
-make -j reseq-core
+make -j8 reseq-core
