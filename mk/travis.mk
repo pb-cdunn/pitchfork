167eed5a05f3bff3182bff85d55a55de9f3de806
Mengjuei Hsieh <mhsieh@pacb.com>
2016-03-23 14:03:36 -0700

    adding travis cache

--- mk/travis.mk
+++ mk/travis.mk
@@ -1,8 +1,9 @@
 PREFIX      = ${HOME}/local
+DISTFILES   = ${HOME}/distfiles
+CCACHE_DIR  = ${HOME}/.ccache
 CC          = gcc-4.8
 CXX         = g++-4.8
 FC          = gfortran-4.8
-CCACHE_DIR  = ${HOME}/.ccache
 HAVE_PYTHON =
 HAVE_CMAKE  =
 HAVE_HDF5   = ${HOME}/hdf5-1.8.13-linux-x86_64-shared
