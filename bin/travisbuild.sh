1a1a958173af74766d105a50d4440249bdd64ea2
Mengjuei Hsieh <mhsieh@pacb.com>
2016-04-12 09:43:57 -0700

    using 5 threads to build, github don't hate me

--- bin/travisbuild.sh
+++ bin/travisbuild.sh
@@ -12,6 +12,6 @@ test -e $HOME/distfiles/boost_1_56_0.tar.gz \
 tar zxf $HOME/distfiles/boost_1_56_0.tar.gz --strip=1 -C $HOME/boost_1_56_0/include/ boost_1_56_0/boost
 cp mk/travis.mk settings.mk
 make init
-make -j1 reseq-core
-make -j1 pbfalcon
+make -j5 reseq-core
+make -j5 pbfalcon
 rm -rf $HOME/local.pip/wheel
