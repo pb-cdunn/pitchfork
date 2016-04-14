3ce4d7edf1d81be5f5f1a0b439b63b3abe861592
Mengjuei Hsieh <mhsieh@pacb.com>
2016-04-14 16:09:06 -0700

    debug

--- bin/travisbuild.sh
+++ bin/travisbuild.sh
@@ -12,6 +12,6 @@ test -e $HOME/distfiles/boost_1_56_0.tar.gz \
 tar zxf $HOME/distfiles/boost_1_56_0.tar.gz --strip=1 -C $HOME/boost_1_56_0/include/ boost_1_56_0/boost
 cp mk/travis.mk settings.mk
 make init
-make -j2 reseq-core
-make -j2 pbfalcon
+make reseq-core
+make pbfalcon
 rm -rf $HOME/local.pip/wheel
